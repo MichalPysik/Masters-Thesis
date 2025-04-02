4ba4a011185083cb322f3fbb67323cc935978c5c
Michal <n3r0x541@gmail.com>
2025-04-02 16:00:15 +0200

    Both embedding benchmarks can run

---
+++ model_benchmarks/embedding_benchmarks/data.py
@@ -0,0 +1,293 @@
+import torch
+from transformers import CLIPProcessor, CLIPModel, AutoProcessor, AutoModel
+from pymilvus import connections, Collection, utility, FieldSchema, DataType, CollectionSchema
+import os
+import pandas as pd
+from PIL import Image
+import json
+
+# Converts traffic sign codes to general class names
+def convert_code_to_general_class(code):
+    """
+    Convert the code to a general class name.
+    """
+    if code.startswith("A"):
+        return "Warning traffic sign"
+    elif code.startswith("B"):
+        return "Prohibitory traffic sign"
+    elif code.startswith("C"):
+        return "Mandatory traffic sign"
+    elif code.startswith("E"):
+        return "Supplementary plate traffic sign"
+    elif code.startswith("IJ"):
+        return "Other informative traffic sign"
+    elif code.startswith("IP"):
+        return "Operational information sign for traffic"
+    elif code.startswith("IS"):
+        return "Directional informative traffic sign"
+    elif code.startswith("IZ"):
+        return "Informative zone traffic sign"
+    elif code.startswith("P"):
+        return "Traffic sign regulating priority"
+    elif code in ["MOST", "RADAR", "Z3", "Z4a", "ZRCADLO"]:
+        return "Transportation equipment"
+    else:
+        raise ValueError(f"Unknown code: {code}")
+
+# Maps traffic sign codes to specific class names
+CODES_TO_NAMES = {
+    "A10": "Traffic semaphore warning traffic sign",
+    "A11": "Pedestrian crossing warning traffic sign",
+    "A12a": "Pedestrians warning traffic sign",
+    "A12b": "Children warning traffic sign",
+    "A13": "Farm animals (cow) warning traffic sign",
+    "A14": "Wild animals (deer) warning traffic sign",
+    "A15": "Roadworks warning traffic sign",
+    "A18": "Falling rocks warning traffic sign",
+    "A19": "Cyclists warning traffic sign",
+    "A1a": "Sharp curve to the right warning traffic sign",
+    "A1b": "Sharp curve to the left warning traffic sign",
+    "A22": "Other danger warning traffic sign",
+    "A24": "Slippery ice warning traffic sign",
+    "A28": "Soft verge warning traffic sign",
+    "A29": "Level crossing with gates or barriers warning traffic sign",
+    "A2a": "Double curve, first to the right warning traffic sign",
+    "A2b": "Double curve, first to the left warning traffic sign",
+    "A30": "Level crossing without gates or barriers warning traffic sign",
+    "A31a": "Distance marker (240 m) warning traffic sign",
+    "A31b": "Distance marker (160 m) warning traffic sign",
+    "A31c": "Distance marker (80 m) warning traffic sign",
+    "A32a": "Warning cross for single-track railway crossing warning traffic sign",
+    "A32b": "Warning cross, multiple-track railway crossing warning traffic sign",
+    "A5a": "Steep descent warning traffic sign",
+    "A5b": "Steep ascent warning traffic sign",
+    "A6a": "Road narrows from both sides warning traffic sign",
+    "A6b": "Road narrows from the right side warning traffic sign",
+    "A7a": "Uneven road warning traffic sign",
+    "A7b": "Speed bumps warning traffic sign",
+    "A8": "Slippery road warning traffic sign",
+    "A9": "Two-way traffic warning traffic sign",
+
+    "B1": "No entry for vehicles (both directions) prohibitory traffic sign",
+    "B11": "No motor vehicles prohibitory traffic sign",
+    "B12": "No entry to the vehicles indicated prohibitory traffic sign",
+    "B13": "No vehicles heavier than indicated prohibitory traffic sign",
+    "B14": "No vehicles with axle weight greater than indicated prohibitory traffic sign",
+    "B15": "No vehicles wider than indicated prohibitory traffic sign",
+    "B16": "No vehicles taller than indicated prohibitory traffic sign",
+    "B17": "No vehicles or combinations longer than indicated prohibitory traffic sign",
+    "B2": "No entry for vehicles prohibitory traffic sign",
+    "B20a": "Speed limit prohibitory traffic sign",
+    "B20b": "End of speed limit prohibitory traffic sign",
+    "B21a": "No overtaking prohibitory traffic sign",
+    "B21b": "End of no-overtaking zone prohibitory traffic sign",
+    "B23a": "No use of audible warning signals prohibitory traffic sign",
+    "B24a": "No right turn prohibitory traffic sign",
+    "B24b": "No left turn prohibitory traffic sign",
+    "B26": "End of all prohibitions prohibitory traffic sign",
+    "B28": "No stopping and no parking prohibitory traffic sign",
+    "B29": "No parking prohibitory traffic sign",
+    "B30": "No pedestrians prohibitory traffic sign",
+    "B32": "Other prohibition (Driving through forbidden) prohibitory traffic sign",
+    "B34": "Minimum distance between vehicles prohibitory traffic sign",
+    "B3b": "No cars prohibitory traffic sign",
+    "B4": "No lorries prohibitory traffic sign",
+    "B5": "No buses prohibitory traffic sign",
+
+    "C1": "Roundabout mandatory traffic sign",
+    "C2a": "Proceed straight mandatory traffic sign",
+    "C3a": "Turn right here mandatory traffic sign",
+    "C3b": "Turn left here mandatory traffic sign",
+    "C4a": "Pass on right mandatory traffic sign",
+    "C4c": "Pass on left or right mandatory traffic sign",
+    "C7a": "Pedestrian path mandatory traffic sign",
+    "C8a": "Cycleway mandatory traffic sign",
+    "C9a": "Shared pedestrian and cycle path mandatory traffic sign",
+
+    "E1": "Number supplementary plate traffic sign",
+    "E13": "Text or symbol supplementary plate traffic sign",
+    "E14": "Transit supplementary plate traffic sign",
+    "E2b": "Intersection shape supplementary plate traffic sign",
+    "E2d": "The shape of two intersections supplementary plate traffic sign",
+    "E3a": "Distance supplementary plate traffic sign",
+    "E3b": "Distance supplementary plate traffic sign",
+    "E4": "Length of section supplementary plate traffic sign",
+    "E5": "Maximum permissible weight supplementary plate traffic sign",
+    "E6": "In the wet (in the rain) supplementary plate traffic sign",
+    "E7a": "Directional arrow for straight direction supplementary plate traffic sign",
+    "E7b": "Directional arrow for turning supplementary plate traffic sign",
+    "E8a": "Start of section supplementary plate traffic sign",
+    "E8b": "Course of the section supplementary plate traffic sign",
+    "E8c": "End of section supplementary plate traffic sign",
+    "E8d": "Validity period supplementary plate traffic sign",
+    "E8e": "Validity period supplementary plate traffic sign",
+    "E9": "Vehicle type supplementary plate traffic sign",
+
+    "IJ14c": "Campsite for tents and caravans informative traffic sign",
+    "IJ2": "Hospital informative traffic sign",
+    "IJ4b": "Stop marker informative traffic sign",
+    "IJ4c": "Bus stop informative traffic sign",
+    "IJ5": "Information informative traffic sign",
+    "IJ7": "Gas station informative traffic sign",
+    "IJ9": "Technical inspection station informative traffic sign",
+
+    "IP10a": "Dead end road operational information sign for traffic",
+    "IP10b": "Advance about a dead end road operational information sign for traffic",
+    "IP11a": "Parking operational information sign for traffic",
+    "IP11b": "Perpendicular or inclined parking operational information sign for traffic",
+    "IP11c": "Longitudinal parking lot operational information sign for traffic",
+    "IP11g": "Parking lot partial parking on the sidewalk longitudinal operational information sign for traffic",
+    "IP12": "Reserved parking operational information sign for traffic",
+    "IP13a": "Covered parking operational information sign for traffic",
+    "IP13b": "Parking lot with parking disc operational information sign for traffic",
+    "IP13c": "Parking lot with parking meter operational information sign for traffic",
+    "IP19": "Shifting lanes operational information sign for traffic",
+    "IP2": "Slowdown threshold operational information sign for traffic",
+    "IP22": "Change in transport organization operational information sign for traffic",
+    "IP30": "State border operational information sign for traffic",
+    "IP4b": "One-way traffic operational information sign for traffic",
+    "IP5": "Recommended speed operational information sign for traffic",
+    "IP6": "Pedestrian crossing operational information sign for traffic",
+
+    "IS11b": "Directional sign for detour directional informative traffic sign",
+    "IS14": "Borders of the territorial unit directional informative traffic sign",
+    "IS15a": "Other name directional informative traffic sign",
+    "IS16b": "Road number directional informative traffic sign",
+    "IS19a": "Directional sign for cyclists directional informative traffic sign",
+    "IS19b": "Directional sign for cyclists directional informative traffic sign",
+    "IS19c": "Directional sign for cyclists directional informative traffic sign",
+    "IS20": "Signal for cyclists directional informative traffic sign",
+    "IS21a": "Directional sign for cyclists straight directional informative traffic sign",
+    "IS21b": "Directional sign for cyclists left directional informative traffic sign",
+    "IS21c": "Directional sign for cyclists right directional informative traffic sign",
+    "IS21d": "End of the cycling route directional informative traffic sign",
+    "IS22a": "Designation of the name of a street or other public space directional informative traffic sign",
+    "IS22b": "Designation of the name of a street or other public space directional informative traffic sign",
+    "IS22c": "Designation of the name of a street or other public space directional informative traffic sign",
+    "IS22d": "Designation of the name of a street or other public space directional informative traffic sign",
+    "IS22e": "Designation of the name of a street or other public space directional informative traffic sign",
+    "IS22f": "Designation of the name of a street or other public space directional informative traffic sign",
+    "IS24a": "Cultural or tourist destination directional informative traffic sign",
+    "IS24b": "Signpost for a cultural or tourist destination directional informative traffic sign",
+    "IS24c": "Communal goal directional informative traffic sign",
+    "IS3a": "Direction sign with destination straight directional informative traffic sign",
+    "IS3b": "Direction sign with destination left directional informative traffic sign",
+    "IS3c": "Direction sign with destination right directional informative traffic sign",
+    "IS4c": "Direction sign with local destination right directional informative traffic sign",
+    "IS5": "Signpost with a different destination directional informative traffic sign",
+    "IS9a": "Signal before a level crossing directional informative traffic sign",
+    "IS9c": "Traffic light before a restricted intersection directional informative traffic sign",
+
+    "IZ4a": "Town informative zone traffic sign",
+    "IZ4b": "End of town informative zone traffic sign",
+    "IZ5a": "Residential zone informative zone traffic sign",
+    "IZ5b": "End of residential zone informative zone traffic sign",
+    "IZ8a": "Traffic restriction zone informative zone traffic sign",
+    "IZ8b": "End of traffic restriction zone informative zone traffic sign",
+
+    "MOST": "Bridge sign",
+
+    "P1": "Intersection with a secondary road traffic sign regulating priority",
+    "P2": "Main road traffic sign regulating priority",
+    "P4": "Give way! traffic sign regulating priority",
+    "P6": "Stop, give way! traffic sign regulating priority",
+    "P7": "Priority to oncoming vehicles traffic sign regulating priority",
+    "P8": "Priority over oncoming vehicles traffic sign regulating priority",
+
+    "RADAR": "Traffic radar",
+
+    "Z3": "Guidance board transportation equipment",
+    "Z4a": "Directional plate with diagonal stripes with a left slope transportation equipment",
+
+    "ZRCADLO": "Traffic mirror",
+}
+
+# Maps the same in reverse
+NAMES_TO_CODES = {v: k for k, v in CODES_TO_NAMES.items()}
+
+
+def create_collection(name, embedding_dim):
+    print(f"Creating a new collection {name}.")
+
+    # Define the schema for the collection (fields: id, video_name, timestamp, embedding)
+    id_field = FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=True)  # Primary key
+    filename_field = FieldSchema(name="filename", dtype=DataType.VARCHAR, max_length=255)
+    class_field = FieldSchema(name="class", dtype=DataType.VARCHAR, max_length=255)
+    embedding_field = FieldSchema(name="embedding", dtype=DataType.FLOAT_VECTOR, dim=embedding_dim)
+
+    schema = CollectionSchema(
+        fields=[id_field, filename_field, class_field, embedding_field],
+        description="Images for retrieval benchmarking.",
+    )
+    collection = Collection(name=name, schema=schema)
+
+    # Create an index for the embedding field
+    index_params = {
+        "index_type": "IVF_FLAT",
+        "metric_type": "COSINE",
+        "params": {"nlist": 128}
+    }
+    # Create the index for the embedding field
+    collection.create_index(field_name="embedding", index_params=index_params)
+
+    return collection
+
+
+def insert_cars196_data(collection, model, processor, device):
+    dataset_dir = "./datasets/CARS196"
+    df = pd.read_excel(f"{dataset_dir}/stanford_cars_with_class_names.xlsx")
+    
+    for sample in df.iterrows():
+        filename = sample[1]["image"]
+        #class_num = sample[1]["class"]
+        class_name = sample[1]["ture_class_name"]
+
+        # open PIL image
+        img = Image.open(f"{dataset_dir}/cars_train_images/{filename}")
+        # Grayscale images need to be converted to RGB for certain models to work
+        if img.mode != "RGB":
+            print(f"Converting {filename} from {img.mode} to RGB.")
+            img = img.convert("RGB")
+
+        inputs = processor(images=img, return_tensors="pt", padding=True).to(device)
+        with torch.no_grad():
+            image_features = model.get_image_features(**inputs)
+        # Normalize and flatten the embedding
+        image_features /= image_features.norm(p=2, dim=-1, keepdim=True)
+        embedding = image_features.cpu().numpy().flatten()
+
+        data = [[filename], [class_name], [embedding]]
+        collection.insert(data)
+        print(f"Inserted {filename} into Milvus.")
+
+
+def insert_czech_traffic_signs_data(collection, model, processor, device):
+    dataset_dir = "./datasets/czech_traffic_signs"
+    geojson_file = dataset_dir + "/annotations.geojson"
+    with open(geojson_file, "r", encoding="utf8") as f:
+        data = json.load(f)
+        data_features = data["features"]
+
+    for sample in data_features:
+        filename = sample["properties"]["FOTO"]
+
+        # Extract the class names
+        class_names = ""
+        for i in range(1, 11):
+            tab = sample["properties"][f"tab{i}"]
+            if tab != "":
+                class_names += tab + ";"
+        
+        # Open PIL image
+        img = Image.open(f"{dataset_dir}/images/{filename}")
+        inputs = processor(images=img, return_tensors="pt", padding=True).to(device)
+        with torch.no_grad():
+            image_features = model.get_image_features(**inputs)
+        
+        # Normalize and flatten the embedding
+        image_features /= image_features.norm(p=2, dim=-1, keepdim=True)
+        embedding = image_features.cpu().numpy().flatten()
+    
+        data = [[filename], [class_names], [embedding]]
+        collection.insert(data)
+        print(f"Inserted {filename} into Milvus.")
---
+++ model_benchmarks/embedding_benchmarks/run_benchmark.py
@@ -0,0 +1,211 @@
+import torch
+from transformers import CLIPProcessor, CLIPModel, AutoProcessor, AutoModel
+from pymilvus import connections, Collection, utility, FieldSchema, DataType, CollectionSchema
+import os
+import pandas as pd
+from PIL import Image
+import json
+import data
+
+# 0 = CLIP, 1 = SigLIP, 2 = ALIGN, 3 = BLIP - Configure!
+ACTIVE_EMB_MODEL = 0
+EMBEDDING_DIMS = [768, 1152, 640, 256]
+EMB_MODEL_NAMES = ["CLIP", "SigLIP", "ALIGN", "BLIP"]
+
+# 0 = CARS196, 1 = Czech traffic signs - Configure!
+ACTIVE_DATASET = 1
+
+collection_name = None
+if ACTIVE_DATASET == 0:
+    collection_name = "cars196_embedding_benchmark_collection"
+elif ACTIVE_DATASET == 1:
+    collection_name = "czech_traffic_signs_embedding_benchmark_collection"
+else:
+    raise ValueError("Invalid dataset selection. Please choose 0 for CARS196 or 1 for Czech traffic signs.")
+
+
+# Connect to Milvus
+host = os.getenv("MILVUS_HOST", "localhost")
+port = os.getenv("MILVUS_PORT", "19530")
+connections.connect(host="localhost", port="19530")
+
+
+# GPU or CPU
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Load multimodal embedding model
+model, processor = None, None
+if ACTIVE_EMB_MODEL == 0:
+    clip_versions = ["openai/clip-vit-base-patch32", "openai/clip-vit-large-patch14"]
+    model = CLIPModel.from_pretrained(clip_versions[1]).eval().to(device)
+    processor = CLIPProcessor.from_pretrained(clip_versions[1])
+
+elif ACTIVE_EMB_MODEL == 1:
+    siglip_version = "google/siglip-so400m-patch14-384"  # most downloaded version on hugging face transformers
+    model = AutoModel.from_pretrained(siglip_version).eval().to(device)
+    processor = AutoProcessor.from_pretrained(siglip_version)
+
+elif ACTIVE_EMB_MODEL == 2:
+    align_version = "kakaobrain/align-base"
+    model = AutoModel.from_pretrained(align_version).eval().to(device)
+    processor = AutoProcessor.from_pretrained(align_version)
+
+elif ACTIVE_EMB_MODEL == 3:
+    from lavis.models import load_model_and_preprocess
+    processor = [None, None]  # Separate image ([0]) and text ([1]) processors
+    model, processor[0], processor[1] = load_model_and_preprocess(
+        name="blip_feature_extractor", model_type="base", is_eval=True, device=device
+    )
+
+
+# Load collection or create new
+if utility.has_collection(collection_name):
+    print(f"Collection {collection_name} already exists")
+    collection = Collection(name=collection_name)
+    
+    for field in collection.schema.fields:
+        if field.name == "embedding":
+            embedding_dim = int(field.params["dim"])
+            break
+    if embedding_dim != EMBEDDING_DIMS[ACTIVE_EMB_MODEL]:
+        print(f"Embedding dimension mismatch: {embedding_dim} != {EMBEDDING_DIMS[ACTIVE_EMB_MODEL]}")
+        collection.drop()
+        collection = data.create_collection(collection_name, EMBEDDING_DIMS[ACTIVE_EMB_MODEL])
+else:
+    collection = data.create_collection(collection_name, EMBEDDING_DIMS[ACTIVE_EMB_MODEL])
+
+
+# Fill the collection with data if it is empty
+if collection.num_entities == 0:
+    if ACTIVE_DATASET == 0:
+        data.insert_cars196_data(collection, model, processor, device)
+    else: # ACTIVE_DATASET == 1:
+        data.insert_czech_traffic_signs_data(collection, model, processor, device)
+else:
+    print(f"Collection {collection_name} already has {collection.num_entities} entities")
+
+
+def run_cars196_benchmark():
+    # Load CARS196 dataset annotations
+    dataset_dir = "./datasets/CARS196"
+    df = pd.read_excel(f"{dataset_dir}/stanford_cars_with_class_names.xlsx")
+
+    # Get list of all 196 unique class names
+    class_names = list(df["ture_class_name"].unique())
+    class_names.sort()
+
+    # Results will be list of dicts, later saved as JSON
+    benchmark_results = []
+
+    # Query for each class name and collect results (top-k = 25)
+    for i, class_name in enumerate(class_names):
+        inputs = processor(text=class_name, return_tensors="pt", padding=True).to(device)
+        with torch.no_grad():
+            text_features = model.get_text_features(**inputs)
+        # Normalize the embedding
+        text_features /= text_features.norm(p=2, dim=-1, keepdim=True)
+
+        # Convert the text embedding to 1-D numpy array
+        query_embedding = text_features.cpu().numpy().flatten()
+
+        search_results = collection.search(
+            data=[query_embedding],
+            anns_field="embedding",
+            param={"metric_type": "COSINE", "top_k": 25},
+            consistency_level="Strong",
+            output_fields=["class"],
+            limit=25,
+        )
+
+        # Store the predictions
+        predictions = []
+        prev_similarity = 1.0
+        for result in search_results[0]:
+            # Make sure the predictions are sorted by similarity in descending order
+            assert result.distance <= prev_similarity
+            prev_similarity = result.distance
+            predictions.append(result.entity.get("class"))
+
+        # Create dict entry for the current query
+        sample_result = {
+            "query": class_name,
+            "predictions": predictions,
+        }
+        benchmark_results.append(sample_result)
+        print(f"Processed {i + 1}/{len(class_names)}: {class_name}")
+
+    # Save results to JSON file
+    results_dir = "./results"
+    os.makedirs(results_dir, exist_ok=True)
+    results_file = os.path.join(results_dir, f"cars196_{EMB_MODEL_NAMES[ACTIVE_EMB_MODEL]}_results.json")
+    with open(results_file, "w") as f:
+        json.dump(benchmark_results, f, indent=4)
+    print(f"Benchmark results saved to {results_file}")
+        
+
+def run_czech_traffic_signs_benchmark():
+    # All 152 unique class names are already stored (sorted) in the dictionary
+    class_names = data.CODES_TO_NAMES.values()
+
+    # Results will be list of dicts, later saved as JSON
+    benchmark_results = []
+
+    # Query for each class name and collect results (top-k = 25)
+    for i, class_name in enumerate(class_names):
+        inputs = processor(text=class_name, return_tensors="pt", padding=True).to(device)
+        with torch.no_grad():
+            text_features = model.get_text_features(**inputs)
+        # Normalize the embedding
+        text_features /= text_features.norm(p=2, dim=-1, keepdim=True)
+
+        # Convert the text embedding to 1-D numpy array
+        query_embedding = text_features.cpu().numpy().flatten()
+
+        search_results = collection.search(
+            data=[query_embedding],
+            anns_field="embedding",
+            param={"metric_type": "COSINE", "top_k": 25},
+            consistency_level="Strong",
+            output_fields=["class"],
+            limit=25,
+        )
+
+        # Store the predictions
+        predictions = []
+        prev_similarity = 1.0
+        for result in search_results[0]:
+            # Make sure the predictions are sorted by similarity in descending order
+            assert result.distance <= prev_similarity
+            prev_similarity = result.distance
+
+            # Each prediction is a list by itself, since more signs can be present in one image
+            multiclass_prediction = result.entity.get("class").split(";")[:-1]
+            predictions.append(multiclass_prediction)
+
+        # Create dict entry for the current query
+        sample_result = {
+            "query": class_name,
+            "query_code": data.NAMES_TO_CODES[class_name],
+            "predictions": predictions,
+        }
+        benchmark_results.append(sample_result)
+        print(f"Processed {i + 1}/{len(class_names)}: {class_name}")
+
+    # Save results to JSON file
+    results_dir = "./results"
+    os.makedirs(results_dir, exist_ok=True)
+    results_file = os.path.join(results_dir, f"czech_traffic_signs_{EMB_MODEL_NAMES[ACTIVE_EMB_MODEL]}_results.json")
+    with open(results_file, "w") as f:
+        json.dump(benchmark_results, f, indent=4)
+    print(f"Benchmark results saved to {results_file}")
+
+
+
+# Run the benchmark
+collection.load()
+if ACTIVE_DATASET == 0:
+    run_cars196_benchmark()
+elif ACTIVE_DATASET == 1:
+    run_czech_traffic_signs_benchmark()
+else:
+    raise ValueError("Invalid dataset selection. Please choose 0 for CARS196 or 1 for Czech traffic signs.")
